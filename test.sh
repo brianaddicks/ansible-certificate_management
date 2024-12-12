0a406aefd3736859e048e8c45bf4d672087baa20
brianaddicks <brian@addicks.us>
2024-12-12 14:42:36 -0500

    moved handler to task file for acme dns challenge don't want to wait until play is completed, want this cleaned asap

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@ ansible-navigator run ./example.yml \
   --eei quay.io/rh_ee_baddicks/cert-mgmt-ee \
   --penv CLOUDFLARE_TOKEN \
   --penv PALO_API_KEY \
+  --penv ANSIBLE_SSH_USERNAME \
   -i ./test-inventory.yml --pp missing #-m stdout
